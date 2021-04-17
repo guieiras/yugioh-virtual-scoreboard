4dec6e3ffdd4531c73621422abd94de22f34a84b
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-04-17 14:52:52 -0300

    feat(lobby): change lobby to be responsible

--- src/Show/Lobby.js
+++ src/Show/Lobby.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import { Link, useLocation } from '@reach/router'
-import QRCode from 'qrcode.react';
-import { Container, Header } from 'semantic-ui-react'
+import QRCode from 'qrcode.react'
+import { Container, Grid, Header } from 'semantic-ui-react'
+import useWindowSize from '../lib/useWindowSize'
 
 const Lobby = ({ uuid }) => {
+  const windowSize = useWindowSize()
   const location = useLocation()
   const hostRoute = `${location.origin}/game/${uuid}`
 
-  return <Container textAlign="center">
-    <Header as='h3' style={{ marginTop: '10px' }} content='Modo de apresentação' />
+  const header = 'Modo de apresentação'
+  const info = 'Entre usando o QR Code acima ou através da seguinte URL'
+
+  return windowSize.height >= window.width ? <Container textAlign="center">
+    <Header as='h3' style={{ marginTop: '10px' }} content={header} />
     <QRCode value={hostRoute} />
-    <p>Entre usando o QR Code acima ou através da seguinte URL</p>
+    <p>{info}</p>
     <Link to={`/game/${uuid}`}>{hostRoute}</Link>
-  </Container>
+  </Container> : <Grid>
+    <Grid.Column width='4'>
+      <QRCode value={hostRoute} size={(windowSize.height || 128)} includeMargin />
+    </Grid.Column>
+    <Grid.Column width='12' verticalAlign='middle'>
+      <Header as='h3' content={header} />
+      <p style={{ margin: 0 }}>{info}</p>
+      <Link to={`/game/${uuid}`}>{hostRoute}</Link>
+    </Grid.Column>
+  </Grid>
 }
 
 export default Lobby;
