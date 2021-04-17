f592466e36ea8a1d0e02851e24bfb7415be1e286
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-04-17 14:25:07 -0300

    feat(vertical): change on layout to make it more responsible

--- src/Show/Presentation/VerticalCards.js
+++ src/Show/Presentation/VerticalCards.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import { Card, Grid, Header } from 'semantic-ui-react'
+import { Grid, Header, Image, Segment } from 'semantic-ui-react'
 
 const VerticalCards = ({ players, style }) => {
   return <Grid style={{ margin: 0, height: '100vh', backgroundColor: style.background }} verticalAlign='middle' columns={1}>
-    <Grid.Column>
-      <Card
-        centered
-        image={players[0].deckUrl}
-        header={<Header as='p' textAlign='center' size='huge'>{players[0].name}</Header>}
-        extra={<Header as='p' textAlign='center' size='large'>{players[0].lp}</Header>}
-      />
-    </Grid.Column>
-    <Grid.Column>
-      <Card centered
-        image={players[1].deckUrl}
-        header={<Header as='p' textAlign='center' size='huge'>{players[1].name}</Header>}
-        extra={<Header as='p' textAlign='center' size='large'>{players[1].lp}</Header>}
-      />
-    </Grid.Column>
+    <Grid.Row verticalAlign='middle' centered>
+      <Grid.Column>
+        <Image centered src={players[0].deckUrl} size='small' bordered />
+        <Segment inverted>
+          <Header as='p' size='huge' textAlign='center'>{players[0].name}</Header>
+          <Header as='p' size='large' textAlign='center'>{players[0].lp}</Header>
+        </Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Image centered src={players[1].deckUrl} size='small' bordered />
+        <Segment inverted>
+          <Header as='p' size='huge' textAlign='center'>{players[1].name}</Header>
+          <Header as='p' size='large' textAlign='center'>{players[1].lp}</Header>
+        </Segment>
+      </Grid.Column>
+    </Grid.Row>
   </Grid>
 }
 
