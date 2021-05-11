89ea634f15fe51c8f4b85c06300afe6db57f2a87
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-05-10 22:53:43 -0300

    feat(screen): change max width on card

--- src/Show/Presentation/Screen/PlayerCard.js
+++ src/Show/Presentation/Screen/PlayerCard.js
@@ -3,7 +3,7 @@ import MatchIcon from '../../MatchIcon'
 
 const PlayerCard = ({ deckUrl, name, lp, match, winWith }) => (
   <div className="ui big rounded image centered" style={{ width: '100%', textAlign: 'center' }}>
-    <Image src={deckUrl} inline style={{ maxWidth: 650 }} />
+    <Image src={deckUrl} inline style={{ maxWidth: 450 }} />
     <div style={{ position: 'absolute', bottom: 0, background: 'rgba(255, 255, 255, 0.8)', width: 'calc(100% - 12px)', textAlign: 'center', padding: 6, left: 6 }}>
       <Header as='p' size='medium' style={{ margin: 5 }}>{name}</Header>
       <Header as='p' size='huge'>{lp}</Header>
