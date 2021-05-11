50d8fb766fb7f461a87b80c57f8a17c29915df2c
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-05-10 22:45:51 -0300

    feat(screen): align margins

--- src/Show/Presentation/Screen/PlayerCard.js
+++ src/Show/Presentation/Screen/PlayerCard.js
@@ -2,9 +2,9 @@ import { Header, Image } from 'semantic-ui-react'
 import MatchIcon from '../../MatchIcon'
 
 const PlayerCard = ({ deckUrl, name, lp, match, winWith }) => (
-  <div className="ui big rounded image centered" style={{ textAlign: 'center' }}>
-    <Image src={deckUrl} inline />
-    <div style={{ position: 'absolute', bottom: 0, background: 'rgba(255, 255, 255, 0.8)', width: '100%', textAlign: 'center', padding: 6 }}>
+  <div className="ui big rounded image centered" style={{ width: '100%', textAlign: 'center' }}>
+    <Image src={deckUrl} inline style={{ maxWidth: 650 }} />
+    <div style={{ position: 'absolute', bottom: 0, background: 'rgba(255, 255, 255, 0.8)', width: 'calc(100% - 12px)', textAlign: 'center', padding: 6, left: 6 }}>
       <Header as='p' size='medium' style={{ margin: 5 }}>{name}</Header>
       <Header as='p' size='huge'>{lp}</Header>
       { match?.length ? <div>
