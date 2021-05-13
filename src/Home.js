f57ffc02d2072aa5faf5e3d199066ecc43570373
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-05-13 20:33:07 -0300

    feat(home): redirect home to show

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import { Button, Grid, Header, Segment } from 'semantic-ui-react'
-import { Link } from '@reach/router'
+import { redirectTo } from '@reach/router'
 
-const Home = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Segment stacked>
-        <Header as='h1' textAlign='center'>
-          Remote Duels
-        </Header>
-        <Grid textAlign='center' style={{ padding: '15px' }}>
-          <Button as={Link} color='green' to='/show'>Novo Jogo</Button>
-        </Grid>
-      </Segment>
-    </Grid.Column>
-  </Grid>
-)
+const Home = () => {
+  React.useEffect(() => {
+    redirectTo('/show')
+  }, []);
+
+  return null;
+}
 
 export default Home;
