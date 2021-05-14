776483f29695ee1f19c8771682aff34ea0b59450
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-05-14 00:47:39 -0300

    feat(lobby): add mirror sync

--- src/Show.js
+++ src/Show.js
@@ -4,18 +4,53 @@ import { v1 as uuidV1 } from 'uuid'
 import { getChannelByGame, getChannelByMirror } from './lib/channel'
 import Presentation from './Show/Presentation'
 import Lobby from './Show/Lobby'
+import { getSyncCode } from './lib/syncCode'
+
+const MIRROR_TIMER = 30
 
 const Show = () => {
   const [uuid, setUuid] = React.useState('')
+  const [mirrorTimer, setMirrorTimer] = React.useState(1)
+  const [mirrorCode, setMirrorCode] = React.useState('')
   const [gameState, setGameState] = React.useState({})
 
-  React.useState(() => {
-    const sessionId = uuidV1()
-    setUuid(sessionId)
-    getChannelByGame(sessionId).subscribe(({ data }) => { setGameState(data) })
-  }, [])
+  React.useEffect(() => {
+    subscribeToUuid(uuidV1())
+    subscribeToMirror()
+  }, []) // eslint-disable-line
 
-  return gameState.players ? <Presentation state={gameState} /> : <Lobby uuid={uuid} />
+  React.useEffect(() => {
+    if (!mirrorCode) return;
+
+    setTimeout(() => {
+      setMirrorTimer(mirrorTimer + 1)
+      if (mirrorTimer % MIRROR_TIMER === 0) { subscribeToMirror() }
+    }, 1000)
+  })
+
+  function subscribeToMirror() {
+    if (mirrorCode) { getChannelByMirror(mirrorCode).unsubscribe() }
+    const newCode = getSyncCode(6)
+    setMirrorCode(newCode)
+    getChannelByMirror(newCode).subscribe(({ data }) => { setMirror(data) })
+  }
+
+  function setMirror(gameId) {
+    getChannelByGame(gameId).unsubscribe()
+    getChannelByMirror(mirrorCode).unsubscribe()
+
+    setMirrorCode(null)
+
+    setGameState({ players: [] })
+    subscribeToUuid(gameId)
+  }
+
+  function subscribeToUuid(gameId) {
+    setUuid(gameId)
+    getChannelByGame(gameId).subscribe(({ data }) => { setGameState(data) })
+  }
+
+  return gameState.players ? <Presentation state={gameState} /> : <Lobby uuid={uuid} mirror={{ timer: MIRROR_TIMER - mirrorTimer % MIRROR_TIMER, code: mirrorCode }} />
 }
 
 export default Show;
--- src/Show/Lobby.js
+++ src/Show/Lobby.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link, useLocation } from '@reach/router'
 import QRCode from 'qrcode.react'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Label, Segment } from 'semantic-ui-react'
 import useWindowSize from '../lib/useWindowSize'
 
-const Lobby = ({ uuid }) => {
+const Lobby = ({ mirror, uuid }) => {
   const windowSize = useWindowSize()
   const location = useLocation()
   const hostRoute = `${location.origin}/game/${uuid}`
 
   const header = 'Modo de apresentação'
+  const mirrorHeader = 'Sincronizar com controle remoto'
+  const mirrorDescription = 'Digite o código abaixo no dispositivo em modo de controle'
   const info = 'Entre usando o QR Code acima ou através da seguinte URL'
 
   return windowSize.height >= windowSize.width ? <Container textAlign="center">
@@ -17,12 +19,23 @@ const Lobby = ({ uuid }) => {
     <QRCode value={hostRoute} />
     <p>{info}</p>
     <Link to={`/game/${uuid}`}>{hostRoute}</Link>
+    <Header as='h3' content={mirrorHeader} />
+    <p style={{ margin: 0 }}>{mirrorDescription}</p>
+    <Segment stacked size='big'>
+      { mirror.code }
+      <Label circular color='grey' style={{ marginLeft: 10 }}>{mirror.timer}</Label>
+    </Segment>
   </Container> : <div style={{ display: 'flex', flexDirection: 'row', verticalAlign: 'middle' }}>
     <QRCode value={hostRoute} size={ windowSize.width ? Math.min(windowSize.width / 4, windowSize.height) : 0} includeMargin />
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
       <Header as='h3' content={header} />
       <p style={{ margin: 0 }}>{info}</p>
       <Link to={`/game/${uuid}`}>{hostRoute}</Link>
+      <Header as='h3' content={mirrorHeader} style={{ marginBottom: 0 }} />
+      <div>
+        {mirror.code}
+        <Label circular color='grey' style={{ float: 'right', marginRight: 5 }}>{mirror.timer}</Label>
+      </div>
     </div>
   </div>
 }
